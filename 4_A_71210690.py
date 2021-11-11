7b93b374e9da15471601d6703206ac2fd5946354
AndreasAndityaTI <93309168+AndreasAndityaTI@users.noreply.github.com>
2021-11-11 20:39:22 +0700

    4_A_71210690

--- 4_A_71210690.py
+++ 4_A_71210690.py
@@ -4,10 +4,10 @@ menu1=int(input("IKAN BAKAR		Rp 25.000,00	: "))
 menu2=int(input("ES DOGER		Rp 6.000,00	: "))
 menu3=int(input("RUJAK CINGUR		Rp 8.000,00	: "))
 print("=====TOTAL=====")
-totalmenu1=menu1*25000
-totalmenu2=menu2*6000
-totalmenu3=menu3*8000
-print("TOTAL IKAN BAKAR		: Rp",totalmenu1)
-print("TOTAL ES DOGER			: Rp",totalmenu2)
-print("TOTAL RUJAK CINGUR		: RP",totalmenu3)
-print("Jumlah total biaya yang harus dibayar adalah Rp",totalmenu1+totalmenu2+totalmenu3)
+tmenu1=menu1*25000
+tmenu2=menu2*6000
+tmenu3=menu3*8000
+print("TOTAL IKAN BAKAR		: Rp",tmenu1)
+print("TOTAL ES DOGER			: Rp",tmenu2)
+print("TOTAL RUJAK CINGUR		: RP",tmenu3)
+print("Jumlah total biaya yang harus dibayar adalah Rp",tmenu1+tmenu2+tmenu3)
