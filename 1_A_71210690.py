c8b708e41809703959fb60f4ae889c2964c520ec
AndreasAndityaTI <93309168+AndreasAndityaTI@users.noreply.github.com>
2021-11-11 20:35:28 +0700

    1_A_71210690

--- 1_A_71210690.py
+++ 1_A_71210690.py
@@ -1,5 +1,5 @@
 phi=3.14
 diameter=int(input("Masukkan diameter : "))
-keliling=phi*diameter
-print("Keliling lingkaran adalah : ",round(keliling,2))
+kel=phi*diameter
+print("Keliling lingkaran adalah : ",round(kel,2))
 
